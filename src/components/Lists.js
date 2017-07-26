32252b445838dd5d311d16e53eebb5086fce19d4
Guanzhou Zhao <benzhao2866@gmail.com>
2017-07-26 21:54:31 +1200

    Add click handler to list menu, fix issue #19

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -3,6 +3,10 @@ import CabinetList from './CabinetList'
 import DimensionList from './DimensionList'
 import QuantityList from './QuantityList'
 export default class Lists extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   getActiveList(activeListName, result) {
     var activeList = <CabinetList />;
     switch (activeListName) {
@@ -23,12 +27,13 @@ export default class Lists extends Component {
     return activeList;
   }
   render() {
+    var handleClick = this.props.handleListChange;
     return (
       <div className="l-content lists">
         <div className="lists-menu">
-            <div className="lists-menu-item">ordered by cabinet</div>
-            <div className="lists-menu-item">ordered by dimension</div>
-            <div className="lists-menu-item">grounped by dimension</div>
+            <div className="lists-menu-item" onClick={() => {handleClick('cab')}}>ordered by cabinet</div>
+            <div className="lists-menu-item" onClick={() => {handleClick('dimension')}}>ordered by dimension</div>
+            <div className="lists-menu-item" onClick={() => {handleClick('quantity')}}>grounped by dimension</div>
         </div>
         {
           this.getActiveList(this.props.activeList, this.props.calcResult)
