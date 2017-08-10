7aeb0d05f1b808028cd281f1edda0bcc8380af3d
Guanzhou Zhao <benzhao2866@gmail.com>
2017-08-10 22:48:44 +1200

    test node.fs in electron

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
+const fs = require('fs');
+
 var element = document.createElement('div');
 document.body.appendChild(element);
 
 render(<App />, element)
+
+fs.readFile('../README.md', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
