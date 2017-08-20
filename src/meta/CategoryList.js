1c82d0ac8dd8fb628aec097ab5ba9df260dc533f
Guanzhou Zhao <benzhao2866@gmail.com>
2017-08-20 22:45:10 +1200

    Show category list

--- src/meta/CategoryList.js
+++ src/meta/CategoryList.js
@@ -7,12 +7,27 @@ import defaultState from './config'
 export default class CategoryList extends Component {
   constructor(props) {
     super(props);
+    this.getCategoryList = this.getCategoryList.bind(this);
   }
-  
+  getCategoryList() {
+    var categoryList = [];
+    return this.props.categories.reduce((accumulator, cat, currentIndex, array)=> {
+      accumulator.push(<div className="category" key={currentIndex}>
+        <div className="category-head">
+          <h3 className="category-name">{cat.name}</h3>
+          <span className="category-desc">{cat.desc}</span>
+        </div>
+        <div className="category-content">
+
+        </div>
+      </div>);
+      return accumulator;
+    }, categoryList);
+  }
   render () {
     return (
       <div>
-
+        {this.getCategoryList()}
       </div>
     )
   }
