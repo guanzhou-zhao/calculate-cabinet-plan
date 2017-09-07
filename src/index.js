9dfe188a466e3aa48749602921eb64119a2c0517
Guanzhou Zhao <benzhao2866@gmail.com>
2017-09-07 22:49:26 +1200

    change back to old version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import App from './components/App'
-import App from './meta/App'
+import App from './components/App'
+// import App from './meta/App'
 
 
 // Try fs in electron
